eb44c073e073c892877aaa5157e655fae3caaca7
Ken Alex Fassone <nexusventuri@gmail.com>
2016-12-08 22:08:10 +0000

    Add sleep for, should block the thread

--- javascript/application.js
+++ javascript/application.js
@@ -12,7 +12,11 @@ Utils = {
     await this._sleep(ms);
     console.log('Two second later');
   }
+  sleepFor: function(sleepDuration){
+      var now = new Date().getTime();
+      while(new Date().getTime() < now + sleepDuration){ /* do nothing */ }
+  }
 }
 
-Utils.sleep(2000);
+Utils.sleepFor(2000);
 console.log('Another console log later');
